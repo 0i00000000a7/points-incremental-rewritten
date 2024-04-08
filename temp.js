fb8a18afac75e78e6117df3e0392abc73b9c5eee
0i00000000a7 <149916146+0i00000000a7@users.noreply.github.com>
2024-04-08 22:26:42 +0800

    Add files via upload

--- temp.js
+++ temp.js
@@ -1,10 +1,1 @@
-const tmp = {
-  sqrt: {
-    get dim_eff() {
-      return player.sqrt.points.add(1).log10().add(1).pow(2)
-    },
-    get galCost() {
-      return E(10).pow(E(100).add(E(50).mul(player.sqrt.galaxies)))
-    }
-  }
-}
+const tmp={sqrt:{get dim_eff(){return window[map+1].player.sqrt.points.add(1).log10().add(1).pow(2)},get galCost(){return E(10).pow(E(100).add(E(50).mul(window[map+1].player.sqrt.galaxies)))}}};
