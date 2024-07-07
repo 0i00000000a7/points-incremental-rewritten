375b1d3fa431c4e653bfa60bd8a6c8d1a827bd35
0i00000000a7 <byd255brkdn@hotmail.com>
2024-07-07 19:57:44 +0800

    nothing happened

--- scripts/script.js
+++ scripts/script.js
@@ -129,7 +129,7 @@ function changeHotkey() {
 function updateResetTime() {
   player.square.resetTime = player.square.resetTime.add(1/30)
 }
-const Endgame = E("e1.3e5")
+const Endgame = E("e143300")
 function isEndgame() {
   return player.points.gte(Endgame)
 }
